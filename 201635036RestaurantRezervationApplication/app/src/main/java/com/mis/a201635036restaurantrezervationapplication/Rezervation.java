40ca50f911c64bf02d5d849469ceb8bfb75d3865
Mert <mert-karaca55@pru.edu.tr>
2021-12-22 21:22:28 +0300

    Restaurant Rezervation Application
    Restaurant reservation application that I created using Android Studio and java language for my Final Project.

---
+++ 201635036RestaurantRezervationApplication/app/src/main/java/com/mis/a201635036restaurantrezervationapplication/Rezervation.java
@@ -0,0 +1,14 @@
+package com.mis.a201635036restaurantrezervationapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Rezervation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rezervation);
+    }
+}
